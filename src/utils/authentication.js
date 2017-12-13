705b68fb45d6508ea5affe0c57fb4631f7976de8
Andrey Khlivnyuk <koytaboyta@gmail.com>
2017-12-14 01:07:13 +0300

    auth config

---
+++ src/utils/authentication.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto-js');
+
+// Constructing signature
+const publicKey = 'MGVlNjY1OWI2NjUxNDAxMDg1NzA0MzUyYjlhN2YwNjc';
+const secretKey = 'NWM4NDQyNTcxZDkzNGNhMWEzMWIwOTc5OTJlZjU0NWMzMTNhNmQyNWM5N2I0OTlmYjI5ODBmMzY4OWY0N2YwNw';
+let timestamp = Math.floor(Date.now() / 1000);
+let payload = timestamp + '.' + publicKey;
+const hash = crypto.HmacSHA256(payload, secretKey);
+const hexHash = crypto.enc.Hex.stringify(hash);
+const signature = payload + '.' + hexHash;
+
+// Exports
+const tickerUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+let options = {
+    'X-signature-id': signature
+};
+
+export {
+    tickerUrl, options
+};
