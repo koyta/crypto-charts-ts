749416e482c13546133445600ccd5874cda6308d
Andrey Khlivnyuk <koytaboyta@gmail.com>
2018-02-14 16:47:55 +0300

    fix typo-bug, now server shoult connect to BA

---
+++ constants.js
@@ -0,0 +1,11 @@
+const ROOT_URL       = 'https://apiv2.bitcoinaverage.com/'
+const TICKER_URL     = 'https://apiv2.bitcoinaverage.com/indices/global/ticker'
+const HISTORICAL_URL = 'https://apiv2.bitcoinaverage.com/indices/global/history/'
+const WEBSOCKET_URL = 'wss://apiv2.bitcoinaverage.com/websocket/'
+
+module.exports = {
+    ROOT_URL,
+    TICKER_URL,
+    HISTORICAL_URL,
+    WEBSOCKET_URL
+}
