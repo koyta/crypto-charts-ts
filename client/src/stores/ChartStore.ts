a4b8bd9f76dab94fd17652ea0ce5a4b2c242d80f
Andrey Khlivnyuk <koytaboyta@gmail.com>
2018-02-18 05:29:47 +0300

    add realtime; small refactoring; change name cData to chartData

--- client/src/stores/ChartStore.ts
+++ client/src/stores/ChartStore.ts
@@ -1,11 +1,14 @@
-import { action, computed, observable, runInAction } from 'mobx';
+import { action, computed, observable, runInAction, IObservable, toJS } from 'mobx';
 import * as helpers from '../utils/gettingData';
 import * as chartjs from 'chart.js';
-import { HistoricalPeriod, HistoryFetchedData, RootStore } from '../interfaces';
+import * as I from '../interfaces';
 
-type ActionState = 'pending' | 'done' | 'error';
+class ChartStore implements I.ChartStoreInterface {
 
-class ChartStore {
+  options?: chartjs.ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: true
+  };
 
   colors: chartjs.ChartColor = [
     'rgba(126, 65, 73, 0.4)',
@@ -18,37 +21,63 @@ class ChartStore {
   ];
   nextColor: number = 0;
 
+  @observable realtimeData: any;
   @observable chartType: chartjs.ChartType;
-  @observable cData: chartjs.ChartData;
-  @observable _historicalPeriod: HistoricalPeriod;
-  state: ActionState; // 'pending' / 'done' / 'error'
+  @observable chartData: chartjs.ChartData;
+  @observable _historicalPeriod: I.HistoricalPeriod;
+  state: I.ActionState; // 'pending' / 'done' / 'error'
 
-  protected rootStore: RootStore;
+  rootStore: I.RootStore;
 
   constructor(rootStore: any) {
     this.rootStore = rootStore;
     this.state = 'pending';
     this.chartType = 'line';
-    this.cData = {
+
+    this.realtimeData = {
       datasets: [
         {
-          data: [3, 2, 1, 4, 2, 5, 1, 8, 7, 10],
-          label: 'Default chart',
+          data: [],
+          label: 'Realtime BTC',
           fill: true
         }
       ],
-      labels: [
-        '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'
-      ]
+      labels: []
+    };
+
+    this.chartData = {
+      datasets: [
+        {
+          data: [],
+          label: 'Chart.js',
+          fill: true
+        }
+      ],
+      labels: []
     };
     this._historicalPeriod = 'alltime';
   }
 
-  @computed get getHistoricalPeriod() {
+  @action updateRealtimeData(newData: I.RealtimeWebsocketData) {
+    try {
+      if (this.realtimeData.datasets.length > 0) {
+        let updatedData = [...this.realtimeData.datasets[0].data, newData.ask];
+        let updatedLabels = [...this.realtimeData.labels, newData.display_timestamp];
+        this.realtimeData.datasets[0].data = updatedData;
+        this.realtimeData.labels = updatedLabels;
+      } else {
+        throw new Error('Dataset is empty');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @computed get historicalPeriod() {
     return this._historicalPeriod;
   }
 
-  @action setHistoricalPeriod(period: HistoricalPeriod) {
+  @action setHistoricalPeriod(period: I.HistoricalPeriod) {
     this._historicalPeriod = period;
   }
 
@@ -58,103 +87,92 @@ class ChartStore {
    */
   @action setChartType(type: chartjs.ChartType) {
     this.chartType = type;
-    console.log('Тип графика теперь ', this.chartType);
   }
 
   /**
-   * Асинхронно забирает данные с сервера bitcoinaverage.com через их API по заданным параметрам.
-   * Возвращает все данные об определенной криптовалюте в заданной валюте
-   * @param {string} crypto
-   * @param {string} currency
-   * @param {string[]} parameters
-   * @returns {Promise<void>}
-   */
-  @action
-  async fetch(crypto: string, currency: string, parameters: string[]) {
-    this.state = 'pending';
-    try {
-      const data = await helpers.getDataAboutCrypto(crypto, currency);
-      // const numbers = [1,2,3,4,5];
-      runInAction('logging fetched data', () => {
-        // console.log(`fetched data: ${JSON.stringify(data, null, '\t')}`);
-        parameters.forEach((value, index) => {
-          console.log(`${value}: ${data[value]}`);
-        });
-        this.cData.datasets[0].data[0] = data.averages.day;
-        this.state = 'done';
-      });
-    } catch {
-      runInAction(() => {
-        this.state = 'error';
-        throw (Error('Не получилось получить данные (fetch в ChartStore). Проверьте подключение к интернету.'));
-      });
-    }
-  }
-
-  /**
-   * Асинхронно забирает исторические данные с сервера bitcoinaverage.com через их API.
-   * Возвращает исторические данные об определенной криптовалюте в заданной валюте за заданный исторический промежуток
-   * @param {string} crypto - криптовалюта
-   * @param {string} currency - валюта
-   * @param {"daily" | "monthly" | "alltime" | number} period - период, за который получаем данные
-   * @returns {Promise<void>}
+   * Asynchronously takes historical data from the server bitcoinaverage.com through their API.
+   * Returns promise of historical data about a particular currency in a given currency for a given historical period
    */
   @action
   async historicalFetch(crypto: string, currency: string, period: '' | 'alltime' | 'daily' | 'monthly') {
     this.state = 'pending';
+    const userStore = this.rootStore.UserStore;
+
     try {
-      const UserStore = this.rootStore.UserStore;
-      let numberOfResults = this.rootStore.UserStore.getNumResults;
-      if (numberOfResults <= 1) {
+      let numberOfResults = userStore.getNumResults;
+      if (numberOfResults <= 0) {
         throw new RangeError('Number of requested results will be 1 or more');
       }
+
+      /**
+       * https://bitcoinaverage.com API sending like ~9k of results e.g. for Bitcoin. We can just slice the data.
+       * // TODO Better if we store all 9k results and dont trying to fetch them again after changing a {userStore.getNumResults}.
+       * But later. There's many crypto's.
+       */
       let fetchedData = [];
-      for (let i = 0; i < UserStore.getCrypto.length; i++) {
-        console.log(`ChartStore| Loading data for crypto: ${UserStore._crypto[i]}.`);
-        fetchedData.push(await helpers.getHistoricalDataAboutCrypto(UserStore.getCrypto[i], UserStore._currency, this.getHistoricalPeriod));
+      for (let i = 0; i < userStore.getCrypto.length; i++) {
+        let data = await helpers.getHistoricalDataAboutCrypto(userStore.getCrypto[i], userStore._currency, this.historicalPeriod);
+        fetchedData.push(data);
       }
+
       let slicedData: Array<any>;
-      slicedData = fetchedData.map((value: void, index: number, array: any) => {
+      slicedData = fetchedData.map((value: any, index: number, array: any) => {
         return array[index].slice(0, numberOfResults).reverse();
       });
-      console.log(slicedData);
 
-      runInAction('Erasing old chart data', () => {
-        this.cData.datasets.splice(0, this.cData.datasets.length);
-        this.cData.labels.splice(0, this.cData.labels.length);
-        this.nextColor = 0;
-      });
+      this.erase();
+      this.prepareDataBeforeAdd(slicedData);
 
-      runInAction('Add new data to chart', () => {
-        slicedData.forEach((data: HistoryFetchedData[], index: number) => {
-          let newData: number[] = data.map((item: HistoryFetchedData) => {
-            return item.average;
-          });
-          let newLabels: string[] = data.map((item: HistoryFetchedData) => {
-              switch (this.getHistoricalPeriod) {
-                case 'alltime':
-                  return item.time.slice(0, 10);
-                case 'monthly' || 'daily':
-                  return item.time.slice(5, 16);
-                default:
-                  return item.time;
-              }
-            }
-          );
-          let newLegend: string = this.rootStore.UserStore.getCrypto[index];
-
-          this.addChartDataToDataset(newData, newLabels, newLegend);
-        });
-      });
       this.state = 'done';
-    } catch (err) {
-      runInAction('Catch error', () => {
+    } catch (error) {
+      runInAction('catch error', () => {
         this.state = 'error';
-        alert(err.message);
+        console.error(`${error.type}: ${error.message}`);
       });
     }
   }
 
+  /** 
+   * Erasing chart data
+   */
+  @action erase() {
+    this.chartData.datasets.splice(0, this.chartData.datasets.length);
+    this.chartData.labels.splice(0, this.chartData.labels.length);
+    this.nextColor = 0;
+  }
+
+  @action prepareDataBeforeAdd(data: Array<I.HistoryFetchedData[]>) {
+    {
+      data.forEach((dataItem: I.HistoryFetchedData[], index: number) => {
+        let newData: number[] = dataItem.map((item: I.HistoryFetchedData) => {
+          return item.average;
+        });
+        let newLabels: string[] = dataItem.map((item: I.HistoryFetchedData) => {
+          switch (this.historicalPeriod) {
+            case 'alltime':
+              return item.time.slice(0, 10);
+            case 'monthly' || 'daily':
+              return item.time.slice(5, 16);
+            default:
+              return item.time;
+          }
+        }
+        );
+        let newLegend: string = this.rootStore.UserStore.getCrypto[index];
+        /**
+         * Pushing created data to chart dataset
+         */
+        this.addChartDataToDataset(newData, newLabels, newLegend);
+      });
+    }
+  }
+
+  /**
+   *
+   * @param {number[]} data
+   * @param {Array<string | string[]>} labels
+   * @param {string} legend
+   */
   @action addChartDataToDataset(data: number[], labels: Array<string | string[]>, legend: string) {
     const newDataset: chartjs.ChartDataSets = {
       data: data,
@@ -163,8 +181,8 @@ class ChartStore {
       hoverBackgroundColor: this.colors[this.nextColor]
     };
     this.nextColor++;
-    this.cData.datasets.push(newDataset);
-    this.cData.labels = labels;
+    this.chartData.datasets.push(newDataset);
+    this.chartData.labels = labels;
   }
 }
 
