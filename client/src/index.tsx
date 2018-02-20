4a9f75332a957963cfda8d03c8f25a54dbe2741b
Andrey Khlivnyuk <koytaboyta@gmail.com>
2018-02-20 16:38:55 +0300

    app index.tsx cleanup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,22 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-// import ChartStore from './stores/ChartStore';
-// import User from './stores/User';
-// import AppStore from './stores/AppStore';
 
 import RootStore from './stores/RootStore';
 
 import { useStrict } from 'mobx';
 import { Provider } from 'mobx-react';
-
-// const stores = {
-//   ChartStore,
-//   User,
-//   AppStore
-// };
-
-// const store = new RootStore();
 
 useStrict(true);
 
